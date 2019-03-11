a0e7883337024ed147fd92abd47b69e697e6096d
421538074 <421538074@qq.com>
2019-03-11 15:52:41 +0800

    js 修改

--- js/backlog.js
+++ js/backlog.js
@@ -1,100 +1,102 @@
-var xm = new Vue({
-    el: "#app",
-    data: {
-        create_at: '',
-        program_id: '',
-        backlog_id:'',
-        titleList: [],
-        imgUrl: "",
-    },
-    methods: {
-        goback() {
-            window.history.back()
-        },
-        add_img(event) {
-            this.program_id = getUrlKey('program_id')
-            this.backlog_id = getUrlKey('backlog_id')
-            var that = this
-            let img1 = event.target.files[0];
-            console.log(img1)
-            let type = img1.type; //文件的类型，判断是否是图片
-            let size = img1.size; //文件的大小，判断图片的大小
-            // if (this.imgData.accept.indexOf(type) == -1) {
-            //     alert('请选择我们支持的图片格式！');
-            //     return false;
-            // }
-            if (size > 3145728) {
-                alert('请选择3M以内的图片！');
-                return false;
-            }
-            let formData = new FormData();
-            formData.append('file', img1);
-            $.ajax({
-                type: "post",
-                url: `${api}/index/api/uploadFile`,
-                data: formData,
-                processData: false,
-                async: false,
-                contentType: false,
-                dataType: "json",
-                success: function (res) {
-                    console.log(res)
-                    sessionStorage.setItem('img', res.data);
-                    window.location.href = `upfile.html?backlog_id=${that.backlog_id}`
-                },
-                error: function (res) {
-                    console.log(res)
-                }
-            });
-        },
-        upChange(event) {
-            $(event.target).find('input.invisible').click();
-        },
-        downloadCodeImg(index) {
-            console.log(index)
-            var img = this.titleList
-            var codeIMG = img[index].data_url
-            console.log(codeIMG)
-            if(codeIMG==""){
-                alert("暂无文件")
-            }else{
-                 var a = document.createElement('a')
-                a.download = name || 'pic'
-                // 设置图片地址
-                a.href = `${api}${codeIMG}`;
-                console.log(a)
-                a.click();
-            }
-           
-        },
-    },
-    filters: {
-        filterTime(time) {
-            var date = new Date(time * 1000);
-            return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-        }
-    },
-    created() {
-        this.program_id = getUrlKey('program_id')
-        this.backlog_id = getUrlKey('backlog_id')
-        var that = this
-        $.ajax({
-            type: "post",
-            url: `${api}/index/api/file_list`,
-            async: true,
-            data: {
-                program_id: this.program_id,
-                backlog_id: this.backlog_id,
-            },
-            dataType: 'json',
-            success: function (res) {
-                console.log(res)
-                that.titleList = res.data
-            },
-            error: function (res) {
-                console.log(res)
-            }
-        });
-    },
+"use strict";
 
-})
+var xm = new Vue({
+  el: "#app",
+  data: {
+    create_at: '',
+    program_id: '',
+    backlog_id: '',
+    titleList: [],
+    imgUrl: "",
+    arr: [{}, {}, {}]
+  },
+  methods: {
+    goback: function goback() {
+      window.history.back();
+    },
+    add_img: function add_img(event) {
+      this.program_id = getUrlKey('program_id');
+      this.backlog_id = getUrlKey('backlog_id');
+      var that = this;
+      var img1 = event.target.files[0];
+      console.log(img1);
+      // var type = img1.type; //文件的类型，判断是否是图片
+
+      var size = img1.size; //文件的大小，判断图片的大小
+      // if (this.imgData.accept.indexOf(type) == -1) {
+      //     alert('请选择我们支持的图片格式！');
+      //     return false;
+      // }
+
+      if (size > 3145728) {
+        alert('请选择3M以内的图片！');
+        return false;
+      }
+
+      var formData = new FormData();
+      formData.append('file', img1);
+      $.ajax({
+        type: "post",
+        url: "".concat(api, "/index/api/uploadFile"),
+        data: formData,
+        processData: false,
+        async: false,
+        contentType: false,
+        dataType: "json",
+        success: function success(res) {
+          console.log(res);
+          sessionStorage.setItem('img', res.data);
+          window.location.href = "upfile.html?backlog_id=".concat(that.backlog_id);
+        },
+        error: function error(res) {
+          console.log(res);
+        }
+      });
+    },
+    upChange: function upChange(event) {
+      $(event.target).find('input.invisible').click();
+    },
+    downloadCodeImg: function downloadCodeImg(index) {
+      console.log(index);
+      var img = this.titleList;
+      var codeIMG = img[index].data_url;
+      console.log(codeIMG);
+
+      if (codeIMG == "") {
+        alert("暂无文件");
+      } else {
+        var a = document.createElement('a');
+        a.download = name || 'pic'; // 设置图片地址
+
+        a.href = "".concat(api).concat(codeIMG);
+        console.log(a);
+        a.click();
+      }
+    }
+  },
+  filters: {
+    filterTime: function filterTime(time) {
+      var date = new Date(time * 1000);
+      return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    }
+  },
+  created: function created() {
+    this.program_id = getUrlKey('program_id');
+    this.backlog_id = getUrlKey('backlog_id');
+    var that = this;
+    $.ajax({
+      type: "post",
+      url: "".concat(api, "/index/api/file_list"),
+      async: true,
+      data: {
+        program_id: this.program_id,
+        backlog_id: this.backlog_id
+      },
+      dataType: 'json',
+      success: function success(res) {
+        console.log(res);
+        that.titleList = res.data;
+      }
+    });
+  }
+});
--- js/requirement.js
+++ js/requirement.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var xm = new Vue({
     el: "#app",
     data: {
@@ -9,150 +11,151 @@ var xm = new Vue({
         content: '',
         isshow: true,
         backlog_id: '',
-        avatar:'',
-        handle_status:'',
+        avatar: '',
+        handle_status: ''
     },
     methods: {
-        goback() {
-            window.history.back()
+        goback: function goback() {
+            window.history.back();
         },
-        upChange(event) {
+        upChange: function upChange(event) {
             $(event.target).find('input.invisible').click();
         },
-        add_img(event) {
-            this.backlog_id = getUrlKey('id')
-            var that =this
-            let img1 = event.target.files[0];
-            console.log(img1)
-            // let type = img1.type; //文件的类型，判断是否是图片
-            let size = img1.size; //文件的大小，判断图片的大小
+        add_img: function add_img(event) {
+            this.backlog_id = getUrlKey('id');
+            var that = this;
+            var img1 = event.target.files[0];
+            console.log(img1); // let type = img1.type; //文件的类型，判断是否是图片
+
+            var size = img1.size; //文件的大小，判断图片的大小
             // if (this.imgData.accept.indexOf(type) == -1) {
             //     alert('请选择我们支持的图片格式！');
             //     return false;
             // }
+
             if (size > 3145728) {
                 alert('请选择3M以内的图片！');
                 return false;
             }
-            let formData = new FormData();
+
+            var formData = new FormData();
             formData.append('file', img1);
             $.ajax({
                 type: "post",
-                url: `${api}/index/api/uploadFile`,
+                url: "".concat(api, "/index/api/uploadFile"),
                 data: formData,
                 processData: false,
                 async: false,
                 contentType: false,
                 dataType: "json",
-                success: function (res) {
-                    console.log(res)
+                success: function success(res) {
+                    console.log(res);
                     sessionStorage.setItem('img', res.data);
-                    window.location.href = `upfile.html?backlog_id=${that.backlog_id}`
-                    // window.location.href ="upfile.html"
+                    window.location.href = "upfile.html?backlog_id=".concat(that.backlog_id); // window.location.href ="upfile.html"
                 },
-                error: function (res) {
-                    console.log(res)
+                error: function error(res) {
+                    console.log(res);
                 }
             });
         },
-        downloadCodeImg(index) {
-            console.log(index)
-            var img = this.Garr
-            var codeIMG = img[index].data_url
-            console.log(codeIMG)
-            if(codeIMG==""){
-                alert("暂无文件")
-            }else{
-                 var a = document.createElement('a')
-                a.download = name || 'pic'
-                // 设置图片地址
-                a.href = `${api}${codeIMG}`;
-                console.log(a)
+        downloadCodeImg: function downloadCodeImg(index) {
+            console.log(index);
+            var img = this.Garr;
+            var codeIMG = img[index].data_url;
+            console.log(codeIMG);
+
+            if (codeIMG == "") {
+                alert("暂无文件");
+            } else {
+                var a = document.createElement('a');
+                a.download = name || 'pic'; // 设置图片地址
+
+                a.href = "".concat(api).concat(codeIMG);
+                console.log(a);
                 a.click();
             }
-           
         },
-        backChange() {
-            this.program_id = getUrlKey('program_id')
-            window.location.href = `projectDetail1.html?program_id=${this.program_id}`
+        backChange: function backChange() {
+            this.program_id = getUrlKey('program_id');
+            window.location.href = "projectDetail1.html?program_id=".concat(this.program_id);
         },
-        focus() {
+        focus: function focus() {
             var myInput = document.getElementById('myInput');
+
             if (myInput == document.activeElement) {
                 // this.isshow = false
                 $(".foot_require").hide();
                 $(".require_foot_three").show();
             }
-
         },
-        send() {
-            this.backlog_id = getUrlKey('id')
-            console.log(this.backlog_id)
-            var that = this
+        send: function send() {
+            this.backlog_id = getUrlKey('id');
+            console.log(this.backlog_id);
+            var that = this;
+
             if (this.text) {
-                var that = this
+                var that = this;
                 $.ajax({
                     type: "post",
-                    url: `${api}/index/api/backlogReplay`,
+                    url: "".concat(api, "/index/api/backlogReplay"),
                     data: {
                         backlog_id: this.backlog_id,
-                        content: this.text,
+                        content: this.text
                     },
                     async: true,
                     dataType: 'json',
-                    success: function (res) {
+                    success: function success(res) {
                         $(".require_foot_three").hide();
-                        $(".foot_require").show();
-                        // var replay_id =res.data.replay_id
+                        $(".foot_require").show(); // var replay_id =res.data.replay_id
+
                         sessionStorage.setItem('replay_id', res.data.replay_id);
-                        that.text = '',
-                            $.ajax({
-                                type: "post",
-                                url: `${api}/index/api/backlogDetail`,
-                                async: true,
-                                data: {
-                                    backlog_id: that.backlog_id,
-                                },
-                                dataType: 'json',
-                                success: function (res) {
-                                    that.Garr = res.data.detail
-                                },
-                                error: function (res) {
-                                    console.log(res)
-                                }
-                            });
-                        this.isshow = true
+                        that.text = '', $.ajax({
+                            type: "post",
+                            url: "".concat(api, "/index/api/backlogDetail"),
+                            async: true,
+                            data: {
+                                backlog_id: that.backlog_id
+                            },
+                            dataType: 'json',
+                            success: function success(res) {
+                                that.Garr = res.data.detail;
+                            },
+                            error: function error(res) {
+                                console.log(res);
+                            }
+                        });
+                        this.isshow = true;
                     },
-                    error: function (res) {
-                        console.log(res)
+                    error: function error(res) {
+                        console.log(res);
                     }
                 });
             } else {
-                alert('请输入内容')
+                alert('请输入内容');
             }
-
         },
-         setHeight(event) {
+        setHeight: function setHeight(event) {
             console.log(event.target.scrollHeight);
-            let bodyFontSize = getComputedStyle(document.querySelector('body')).fontSize;
-            bodyFontSize = bodyFontSize.substring(0,bodyFontSize.length - 2);
-            let textareaHeight = getComputedStyle(event.target).height;
-            textareaHeight = textareaHeight.substring(0,textareaHeight.length - 2);
-            if(parseInt(bodyFontSize)*0.4 == parseInt(textareaHeight) && event.target.style.height.substring(0,event.target.style.height.length - 2) >= bodyFontSize*1) {
+            var bodyFontSize = getComputedStyle(document.querySelector('body')).fontSize;
+            bodyFontSize = bodyFontSize.substring(0, bodyFontSize.length - 2);
+            var textareaHeight = getComputedStyle(event.target).height;
+            textareaHeight = textareaHeight.substring(0, textareaHeight.length - 2);
+
+            if (parseInt(bodyFontSize) * 0.4 == parseInt(textareaHeight) && event.target.style.height.substring(0, event.target.style.height.length - 2) >= bodyFontSize * 1) {
                 return false;
             }
+
             event.target.style.height = 'auto';
             event.target.style.height = event.target.scrollHeight + 'px';
-            if(event.target.style.height.substring(0,event.target.style.height.length - 2) >= bodyFontSize*1) {
-                event.target.style.height = bodyFontSize*1 +'px';
+
+            if (event.target.style.height.substring(0, event.target.style.height.length - 2) >= bodyFontSize * 1) {
+                event.target.style.height = bodyFontSize * 1 + 'px';
             }
         }
-        
     },
     filters: {
-        filterTime(time) {
-            var date = new Date(time * 1000);
-            // var date = new Date();
+        filterTime: function filterTime(time) {
+            var date = new Date(time * 1000); 
             var year = date.getFullYear();
             var month = date.getMonth() + 1;
             var day = date.getDate();
@@ -162,51 +165,48 @@ var xm = new Vue({
             if (day < 10) {
                 day = "0" + day;
             }
-            return  year + "-" + month + "-" + day;
+            return year + "-" + month + "-" + day;
         }
     },
-    created() {
-        this.backlog_id = getUrlKey('id')
-        console.log(this.backlog_id)
-        var that = this
+    created: function () {
+        this.backlog_id = getUrlKey('id');
+        console.log(this.backlog_id);
+        var that = this;
         $.ajax({
             type: "post",
-            url: `${api}/index/api/backlogDetail`,
+            url: "".concat(api, "/index/api/backlogDetail"),
             async: true,
             data: {
-                backlog_id: this.backlog_id,
+                backlog_id: this.backlog_id
             },
             dataType: 'json',
-            success: function (res) {
-                console.log(res)
-                that.avatar =res.data.descr.avatar
-                that.handle_status=res.data.descr.handle_status
-                that.create_at = res.data.descr.create_at
-                that.log_content = res.data.descr.log_content
-                that.log_name = res.data.descr.log_name
-                that.Garr = res.data.detail
-            },
+            success: function success(res) {
+                console.log(res);
+                that.avatar = res.data.descr.avatar;
+                that.handle_status = res.data.descr.handle_status;
+                that.create_at = res.data.descr.create_at;
+                that.log_content = res.data.descr.log_content;
+                that.log_name = res.data.descr.log_name;
+                that.Garr = res.data.detail;
+            }
         });
     }
-})
-
+});
 
 
 $(".require_main").click(function () {
-    $("#myInput").blur()
+    $("#myInput").blur();
     $(".require_foot_three").hide();
     $(".foot_require").show();
-})
+});
 $(".btn-group").click(function () {
-    $("#myInput").blur()
+    $("#myInput").blur();
     $(".require_foot_three").hide();
     $(".foot_require").show();
-})
-
-
-const inputItems = document.querySelectorAll("textarea");
+});
+var inputItems = document.querySelectorAll("textarea");
 inputItems.forEach(function (ele) {
     ele.addEventListener("blur", function () {
-        window.scrollTo(0, 0)
-    })
+        window.scrollTo(0, 0);
+    });
 });

2e3eeaa6c54c46dac117986d1f83284b02ea3d11
421538074 <421538074@qq.com>
2019-03-14 15:17:44 +0800

    info页面增加返回按钮

--- js/info.js
+++ js/info.js
@@ -1,20 +1,21 @@
 var vm = new Vue({
     el: "#app",
     data: {
-        title:'',
-        defend_time:'',
-        wechat:'',
-        wechat_time:'',
-        domain_time:'',
-        domain:'',
-        rate:'',
-        defend_fee:'',
-        server_time:'',
-        server_ip:"",
+        title: '',
+        defend_time: '',
+        wechat: '',
+        wechat_time: '',
+        domain_time: '',
+        domain: '',
+        rate: '',
+        defend_fee: '',
+        server_time: '',
+        server_ip: "",
     },
-    methods: {},
-    components: {
-
+    methods: {
+        goIndex() {
+            window.history.back();
+        }
     },
     filters: {
         filterTime(time) {
@@ -29,7 +30,7 @@ var vm = new Vue({
             if (day < 10) {
                 day = "0" + day;
             }
-            return year + "年" + month + "月" + day +"日";
+            return year + "年" + month + "月" + day + "日";
         }
     },
     created() {
@@ -44,18 +45,17 @@ var vm = new Vue({
             },
             dataType: 'json',
             success: function (res) {
-                that.title=res.data.title
-                that.defend_fee=res.data.defend_fee
-                that.defend_time=res.data.start_time
-                that.wechat=res.data.wechat
-                that.wechat_time=res.data.wechat_time
-                that.domain_time=res.data.domain_time
-                that.domain=res.data.domain
-                that.server_ip=res.data.server_ip
-                that.server_time=res.data.server_time
+                that.title = res.data.title
+                that.defend_fee = res.data.defend_fee
+                that.defend_time = res.data.start_time
+                that.wechat = res.data.wechat
+                that.wechat_time = res.data.wechat_time
+                that.domain_time = res.data.domain_time
+                that.domain = res.data.domain
+                that.server_ip = res.data.server_ip
+                that.server_time = res.data.server_time
             },
-            error: function (res) {
-            }
+            error: function (res) {}
         });
     }
 
